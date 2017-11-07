cbf39f4f87408bb87417f930915e473cfc9f3e05
Gregor Jeraj <gregor@carlock.co>
2017-11-07 16:26:33 +0100

    Updated Arquillian cube version and fixed test configuration

--- src/test/java/com/quickstart/TestHello.java
+++ src/test/java/com/quickstart/TestHello.java
@@ -1,5 +1,7 @@
 package com.quickstart;
 
+import org.arquillian.cube.DockerUrl;
+import org.arquillian.cube.HostPort;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -11,7 +13,8 @@ import org.junit.runner.RunWith;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
@@ -25,11 +28,24 @@ public class TestHello {
                 .addClass(JAXRSConfiguration.class);
     }
 
+    @ArquillianResource
+    @DockerUrl(containerName = "arquillian-cube-wildfly-test", exposedPort = 8080)
+    private URL url;
+
+    @HostPort(containerName = "arquillian-cube-wildfly-test", value = 8080)
+    private int port;
+
     @Test
     @RunAsClient
-    public void test(@ArquillianResource URL url) throws Exception {
+    public void test() throws Exception {
+        URI uri = UriBuilder.fromUri(url.toURI())
+                .port(port)
+                .path("resources")
+                .path("hello")
+                .build();
+
         String hello = ClientBuilder.newClient()
-                .target("http://localhost:58080" + url.getFile() + "resources/hello")
+                .target(uri)
                 .request(MediaType.TEXT_PLAIN)
                 .get()
                 .readEntity(String.class);
