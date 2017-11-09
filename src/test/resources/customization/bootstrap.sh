88dd3adab2bb848e1a736498840df1dcbfeb08f7
Gregor Jeraj <gregor@carlock.co>
2017-11-09 14:47:13 +0100

    Fixed mysql initialization and updated server versions

--- src/test/resources/customization/bootstrap.sh
+++ src/test/resources/customization/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 JBOSS_HOME=/opt/jboss/wildfly
 JBOSS_CLI=${JBOSS_HOME}/bin/jboss-cli.sh
@@ -16,12 +16,28 @@ ${JBOSS_HOME}/bin/standalone.sh &
 echo "=> Waiting for the server to boot"
 wait_for_server
 
-echo "=> Adding MySQL data source: " ${MYSQL_URI}
-echo MYSQL_URI=${MYSQL_URI} > ${JBOSS_HOME}/env.properties
-${JBOSS_CLI} --connect --file=${JBOSS_SCRIPT} --properties=${JBOSS_HOME}/env.properties
+echo "=> Adding MySQL data source: " $MYSQL_URI
+${JBOSS_CLI} -c << EOF
+batch
+# Add MySQL module
+module add --name=com.mysql --resources=/opt/jboss/wildfly/customization/mysql-connector-java-5.1.44.jar --dependencies=javax.api,javax.transaction.api
+
+# Add MySQL driver
+/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource)
+
+# Add the datasource
+data-source add --name=mysqlDS --driver-name=mysql --jndi-name=java:jboss/datasources/TestDS --connection-url=jdbc:mysql://$MYSQL_URI/test?characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull --user-name=root --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+
+# Execute the batch
+run-batch
+EOF
 
 echo "=> Shutting down WildFly"
 ${JBOSS_CLI} -c ":shutdown"
 
 # Fix for configuration rename issue
 rm -rf ${JBOSS_HOME}/standalone/configuration/standalone_xml_history
+
+# Restart wildfly
+echo "=> Restarting WildFly"
+${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
